130ed0a532dcaff42692d8c850b41fd6d975bd14
AndrewG <54869954+Andrew-Gehman@users.noreply.github.com>
2020-10-01 19:00:34 -0500

    All the changes

---
+++ src/main/java/model/Data.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Data
+{
+
+    private ArrayList<Room> rooms; //collection of class room
+    private ArrayList<ClassTimes> times;
+    private ArrayList<Course> courses;
+    private ArrayList<String> classroomPreferences;
+    private ArrayList<Professor> professors;
+    private ArrayList<Department> departments;
+    private String location;
+
+
+    public Data(String filename)
+    {
+
+    }
+}
