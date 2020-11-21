e3526133570ef5abe69d189966824aa6883a4efd
AndrewG <54869954+Andrew-Gehman@users.noreply.github.com>
2020-11-20 19:17:54 -0600

    remove old print method

--- src/main/java/controller/OutputScheduleController.java
+++ src/main/java/controller/OutputScheduleController.java
@@ -225,24 +225,7 @@ public class OutputScheduleController {
             textArea.appendText(error);
         }
     }
-    private void print(Node node)
-    {
-        System.out.println("Creating a printer job...");
-
-        PrinterJob job = PrinterJob.createPrinterJob();
-        if (job != null) {
-            System.out.println(job.jobStatusProperty().asString());
-
-            boolean printed = job.printPage(node);
-            if (printed) {
-                job.endJob();
-            } else {
-                System.out.println("Printing failed.");
-            }
-        } else {
-            System.out.println("Could not create a printer job.");
-        }
-    }
+   
 
 
 }
